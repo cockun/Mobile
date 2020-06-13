26847a40017b87632136b0565381c64e2a0737c9
Quang <concockun@gmail.com>
2020-06-13 15:41:58 +0700

    coc

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Item } from './conponents/Item';
+
+export default function App() {
+  return (
+    <View>
+      <Item />
+      <Item />
+      <Item />
+    </View>
+  );
+}
---
+++ conponents/detail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function Detail() {
+  return (
+    <View>
+        <Image source={}/>
+
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
